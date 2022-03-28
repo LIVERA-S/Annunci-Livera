2dde8cc26d546c6f2232c9031951b6da0efe2b55
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-03-28 07:43:25 +0000

    punto3

---
+++ annunci-livera/src/app/annunci.model.ts
@@ -0,0 +1,3 @@
+export class Annunci{
+    constructor (public name : string, public number: Number, public message?: string) {}
+}
--- annunci-livera/src/app/app.component.ts
+++ annunci-livera/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Annunci } from '../app/annunci.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
